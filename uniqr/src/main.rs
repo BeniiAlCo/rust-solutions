49db9ca321e118226eeeb7ebde1a055446529252
Benii <benii.alco@gmail.com>
2022-06-06 22:03:48 +0100

    Functionality to meet tests for GNU version of uniq.

--- uniqr/src/main.rs
+++ uniqr/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::ExitCode;
+
+#[allow(dead_code)]
+fn main() -> ExitCode {
+    if let Err(err) = uniqr::get_args().and_then(uniqr::run) {
+        eprintln!("{err}");
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
+    }
 }
